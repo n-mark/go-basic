5225578274898e6bb6838bb5037bd8ca4df09b64
{n-mark} <{markovskinikita@gmail.com}>
2026-03-22 09:30:50 +0300

    fix: chessboard size console scan

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
-    var size int = 8;
+    var size int
+    fmt.Print("Введите размер доски: ")
+    fmt.Scan(&size)
+
     for i := 0; i < size; i++ {
         for j := 0; j < size; j++ {
             if ((i+j) % 2 == 0) {
